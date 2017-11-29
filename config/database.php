fa594e8a47324b702457ea46ee859b4c77f3251f
Unknown <marianmrva123@gmail.com>
2017-11-29 15:33:01 +0100

    changed name db, added socket for linux

--- config/database.php
+++ config/database.php
@@ -43,11 +43,12 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'zooDB'),
+            'database' => env('DB_DATABASE', 'zoodb'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
-//            'unix_socket' => env('DB_SOCKET', ''),
+//            'unix_socket' => env('DB_SOCKET', ''), // default
             'unix_socket' => "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock", // todo what about windows?
+//            'unix_socket' => "/opt/lampp/var/mysql/mysql.sock", // linux
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
